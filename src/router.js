ec53509dd75bb66d504265963267726467f81796
Rafael Castillo <rafaelcastillo@outlook.com>
2019-08-29 17:25:51 -0500

    Completed authentication feature

--- src/router.js
+++ src/router.js
@@ -2,9 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
+import Auth from '@okta/okta-vue'
+
+import Hello from './components/HelloWorld'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-923564.okta.com/oauth2/default',
+  client_id: '0oa11617ua0dhaQTQ357',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
+
 Vue.use(Router)
 
-export default new Router({
+let router =  new Router({
   routes: [
     {
       path: '/',
@@ -18,6 +29,19 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    },
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
   ]
 })
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router
